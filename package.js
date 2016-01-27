fb590a4a7431f61d4d9a73fd876daa7ce5a8efa1
Steve Steinitz <steve@dtctc.com>
2016-01-27 17:23:36 +1100

    minor change to trigger travis

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'steinitz:react-es6-autobind',
-  version: '0.1.1',
-  // Brief, one-line summary of the package.
-  summary: "For all methods, binds 'this' to the React component created with class extends.",
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/steinitz/Meteor-React-ES6-AutoBind',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+	name: 'steinitz:react-es6-autobind',
+	version: '0.1.1',
+	// Brief, one-line summary of the package.
+	summary: "For all methods, binds 'this' to the React component created with class extends.",
+	// URL to the Git repository containing the source code for this package.
+	git: 'https://github.com/steinitz/Meteor-React-ES6-AutoBind',
+	// By default, Meteor will default to using README.md for documentation.
+	// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('react-es6-autobind.js');
-  api.export('SteinitzREA', ['client', 'server']);
+	api.versionsFrom('1.2.1');
+	api.use('ecmascript');
+	api.addFiles('react-es6-autobind.js');
+	api.export('SteinitzREA', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('steinitz:react-es6-autobind');
-  api.addFiles('react-es6-autobind-tests.js');
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('steinitz:react-es6-autobind');
+	api.addFiles('react-es6-autobind-tests.js');
 });
