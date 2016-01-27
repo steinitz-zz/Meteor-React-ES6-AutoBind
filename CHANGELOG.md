6139e07cefbff796c10c135eda9a17b9c0113eea
Steve Steinitz <steve@dtctc.com>
2016-01-27 17:45:48 +1100

    Added dependency on React to tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,13 @@
 # Change Log
-All notable changes to this project will be documented in this file.
+Notable changes to this project will be documented here.
 
 ## [Unreleased]
 ### Added
 - nothing yet.
+
+## [0.1.2] - 26 Jan 2016
+### Added
+- Added dependency on React to tests
 
 ## [0.1.1] - 26 Jan 2016
 ### Added
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'steinitz:react-es6-autobind',
-	version: '0.1.1',
+	version: '0.1.2',
 	// Brief, one-line summary of the package.
 	summary: "For all methods, binds 'this' to the React component created with class extends.",
 	// URL to the Git repository containing the source code for this package.
@@ -12,14 +12,13 @@ Package.describe({
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.2.1');
-	api.use('ecmascript');
+	api.use(['ecmascript', 'react@0.14.1_1']);
 	api.addFiles('react-es6-autobind.js');
 	api.export('SteinitzREA', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
-	api.use('ecmascript');
-	api.use('tinytest');
+	api.use(['ecmascript', 'tinytest', 'react@0.14.1_1']);
 	api.use('steinitz:react-es6-autobind');
 	api.addFiles('react-es6-autobind-tests.js');
 });
